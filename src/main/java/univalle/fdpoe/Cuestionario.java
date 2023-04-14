e6f5c84fefff92d28e43b1987acf2f670bdf7b76
SamuelOrtizS <samuelortiztab@gmail.com>
2023-04-13 19:23:01 -0500

    Initial Commit

---
+++ src/main/java/univalle/fdpoe/Cuestionario.java
@@ -0,0 +1,4 @@
+package univalle.fdpoe;
+
+public class Cuestionario {
+}
---
+++ src/main/java/univalle/fdpoe/Main.java
@@ -0,0 +1,8 @@
+package univalle.fdpoe;
+public class Main {
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+
+        };
+    }
+}
